143ae0d85276d15f79f23d2a83b3fed6d40489f9
Memo Chou <memochou1993@gmail.com>
2024-07-31 23:54:32 +0800

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
+      'eol-last': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'object-curly-spacing': ['error', 'always'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+    },
+  },
+];
