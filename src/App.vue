e7eea212442cd6519e79eb5da549075d9ec1e41c
Memo Chou <memochou1993@gmail.com>
2024-08-04 20:07:05 +0800

    Update imports

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import AppFooter from '@/components/AppFooter.vue';
-import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
 </script>
 
 <template>
