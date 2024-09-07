efa94b74dd52213c8f3d1d5f9da2673fedc24969
Memo Chou <memochou1993@gmail.com>
2024-09-07 23:59:50 +0800

    Add firebase auth

---
+++ src/firebase/auth.js
@@ -0,0 +1,23 @@
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from './app';
+
+const auth = getAuth(app);
+
+export const signUp = ({ email, password }) => createUserWithEmailAndPassword(auth, email, password);
+
+export const signIn = ({ email, password }) => signInWithEmailAndPassword(auth, email, password);
+
+export const signOut = () => auth.signOut();
+
+export const onAuthStateChanged = (callback) => auth.onAuthStateChanged(callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      resolve(user);
+      unsubscribe();
+    }, (error) => {
+      reject(error);
+    });
+  });
+};
